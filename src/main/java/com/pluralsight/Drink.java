f90fe574526d91d58bc8215ef43231c24f38f8da
meron37 <134938986+meron37@users.noreply.github.com>
2025-11-10 09:14:16 -0600

    i have created my interface and drink class

---
+++ src/main/java/com/pluralsight/Drink.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import java.math.BigDecimal;
+
+public class Drink implements OrderItem {
+
+    private String name;
+    private BigDecimal price;
+
+    public Drink(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
